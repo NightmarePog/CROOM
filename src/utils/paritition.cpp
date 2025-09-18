0027e762320e0a7c985d2cf8758f412e57d41551
Nightmare Pog <44711407+NightmarePog@users.noreply.github.com>
2025-09-18 23:01:01 +0200

    chore: delete unnecesary comments/prints

--- src/utils/paritition.cpp
+++ src/utils/paritition.cpp
@@ -1,16 +1,13 @@
 #include "utils/line_segment.hpp"
-#include <iostream>
 #include <stdexcept>
 
 
 Partition::Partition(LineSegment partition) {
-    std::cout << "CREATING PARTITION:" << partition.pos_a.x << "," << partition.pos_a.y << " " << partition.pos_b.x << "," << partition.pos_b.y << std::endl;
-   this->partition = partition;
+this->partition = partition;
 }
 
 
 Side Partition::decide_side(LineSegment segment) {
-    std::cout << "BACK:" << is_in_back(segment) << "FRONT:"<< is_in_front(segment) << std::endl;
     if (is_in_front(segment)) {
         return Side::FRONT;
     }
@@ -24,7 +21,7 @@ Side Partition::decide_side(LineSegment segment) {
 
 }
 
-// MADE WRONG
+
 bool Partition::is_in_front(LineSegment segment) {
     auto side = [](const Vec2& p, const LineSegment& line) {
         return (line.pos_b.x - line.pos_a.x) * (p.y - line.pos_a.y) -
@@ -33,19 +30,16 @@ bool Partition::is_in_front(LineSegment segment) {
 
     float d1 = side(segment.pos_a, this->partition);
     float d2 = side(segment.pos_b, this->partition);
-    std::cout << "D1 and D2: " << d1 << d2 << std::endl;
     return d1 > 0 && d2 > 0;
 }
 
 
-// MADE WRONG
+
 bool Partition::is_in_back(LineSegment segment) {
     auto side = [](const Vec2& p, const LineSegment& line) {
         return (line.pos_b.x - line.pos_a.x) * (p.y - line.pos_a.y) -
                (line.pos_b.y - line.pos_a.y) * (p.x - line.pos_a.x);
     };
-    std::cout << "SEGMENT POSITION:" <<segment.pos_a.x << "," << segment.pos_a.y << std::endl;
-    std::cout << "PARTITION POSITION:" << partition.pos_a.x << "," << partition.pos_a.y << std::endl;
     float d1 = side(segment.pos_a, this->partition);
     float d2 = side(segment.pos_b, this->partition);
 
