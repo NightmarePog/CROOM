23d2c972dec659258e7980502f86c5305d6354d6
Ari <109524755+Pleushy@users.noreply.github.com>
2025-09-26 16:39:09 +0200

    fix: changed vector to queue, formatted code for readability

--- src/core/user_input.cpp
+++ src/core/user_input.cpp
@@ -5,37 +5,50 @@
 #include <SDL_scancode.h>
 #include <cstddef>
 #include <cstdlib>
-#include <vector>
+#include <queue>
 
 UserInputService::UserInputService() {};
 
-const UserInput inputs[] = {UserInput::QUIT, UserInput::W, UserInput::A, UserInput::S, UserInput::D, UserInput::ARROW_LEFT, UserInput::ARROW_RIGHT};
+// List of possible user inputs
+const UserInput inputs[] = {
+    UserInput::QUIT,
+    UserInput::W,
+    UserInput::A,
+    UserInput::S,
+    UserInput::D,
+    UserInput::ARROW_LEFT,
+    UserInput::ARROW_RIGHT
+};
 
-std::vector<UserInput> UserInputService::poll_input_event() {
-    std::vector<UserInput>output;
+std::queue<UserInput> UserInputService::poll_input_event() {
+    std::queue<UserInput>output;
 
     while (SDL_PollEvent(&this->event)) {
         if (event.type == SDL_QUIT) {
-            output = {inputs[0]};
+            output.push(inputs[0]);
             return output;
         }
     }
 
     const Uint8* keystates = SDL_GetKeyboardState(NULL);
-    const Uint8 scans[] = {keystates[SDL_SCANCODE_Q], keystates[SDL_SCANCODE_W], keystates[SDL_SCANCODE_A], keystates[SDL_SCANCODE_S], keystates[SDL_SCANCODE_D], keystates[SDL_SCANCODE_LEFT], keystates[SDL_SCANCODE_RIGHT]};
+    const Uint8 scans[] = {
+        keystates[SDL_SCANCODE_Q],
+        keystates[SDL_SCANCODE_W],
+        keystates[SDL_SCANCODE_A],
+        keystates[SDL_SCANCODE_S],
+        keystates[SDL_SCANCODE_D],
+        keystates[SDL_SCANCODE_LEFT],
+        keystates[SDL_SCANCODE_RIGHT]
+    };
 
-    int added = 0;
     int i = 0;
     for (int scan : scans) {
         if (scan) {
-            output.resize(added+1);
-            output[added] = inputs[i];
-            added++;
+            output.push(inputs[i]);
         }
         i++;
     }
-    output.resize(added+1);
-    output[added] = UserInput::UNDEFINED;
+    output.push(UserInput::UNDEFINED);
 
     return output;
 }
